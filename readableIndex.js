572c6b402d479494ef8e35d04bfdd94e544412ae
esponges <esponges@gmail.com>
2023-06-22 13:21:51 -0600

    async reading

--- readableIndex.js
+++ readableIndex.js
@@ -5,9 +5,22 @@ const readableStream = fs.createReadStream('readable.txt');
 let data = '';
 /* data event is used to get the data chunks and concatanate it to data variable */
 readableStream.on('data', (chunk) => {
-    data += chunk;
+  data += chunk;
 });
 /* end event is used here to log the data in console when reading is completed */
 readableStream.on('end', () => {
     console.log(data);
 });
+
+// read asynchronously the stream
+async function readStream() {
+  const readableStream = fs.createReadStream('readable.txt');
+  data = '';
+  for await (const chunk of readableStream) {
+    console.log('reading async chunk', chunk);
+    data += chunk;
+  }
+  console.log(data);
+}
+
+readStream();
